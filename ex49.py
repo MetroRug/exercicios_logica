7fd866732f9ddac4957b40ce332f6e3fded7417d
MetroRug <74714365+MetroRug@users.noreply.github.com>
2022-03-10 18:00:31 -0300

    Atualizações e correções

--- ex49.py
+++ ex49.py
@@ -3,17 +3,16 @@
 são pares e quantos são ímpares.
 
 '''
-
 n1 = 0
-n2 = n1%2
 total_par = 0
 total_impar = 0
 
-while n1 <= 6:
-    n1 += 1
-    if n2 == 0:
+while n1<6:
+    n2 = int(input('Digite um número: '))
+    if n2%2 == 0:
         total_par += 1
     else:
         total_impar += 1
+    n1 += 1
 
-print(f"Temos {total_par} números pares e {total_impar} números impares.")
+print(f'De {n1} números, temos {total_par} números pares e {total_impar} números impares')
